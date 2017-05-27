51c5151c7de91c5493d35c156b1bfcd2dc84cca2
pankajchuahan <pankaj3960x@gmail.com>
2017-05-27 15:25:27 +0530

    Content script added in web Extensions

---
+++ hoopoe.js
@@ -0,0 +1,7 @@
+(function(){
+  var e = document.createElement("div");
+  e.style = "position:fixed;bottom:0;right:0;left:0;height:22px;background:#07c;z-index:999999999999999999";
+  var s = document.body;
+  s.style = "margin-bottom:22px";
+  s.insertBefore(e, s.firstChild);
+})();
