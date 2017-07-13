a07234b9c8e2656dd5c87a2b69ddb92ffa8c6e1b
Pankaj Chauhan <pankaj3960x@gmail.com>
2017-07-13 16:04:30 +0530

    Added Typescript to Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-class App extends Component {
+const logo = require('./logo.svg');
+
+class App extends React.Component<{}, {}> {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2><span>Welcome to React</span></h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          <span>To get started, edit <code>src/App.js</code> and save to reload.</span>
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
