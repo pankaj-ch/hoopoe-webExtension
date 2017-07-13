d86fc3afe79f62e9bfb40ab66aa03cd1fc580ee0
Pankaj Chauhan <pankaj3960x@gmail.com>
2017-07-13 17:22:21 +0530

    Revert "Added Typescript to Project"
    This reverts commit a07234b9c8e2656dd5c87a2b69ddb92ffa8c6e1b.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const logo = require('./logo.svg');
-
-class App extends React.Component<{}, {}> {
+class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2><span>Welcome to React</span></h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <span>To get started, edit <code>src/App.js</code> and save to reload.</span>
         </p>
       </div>
     );
